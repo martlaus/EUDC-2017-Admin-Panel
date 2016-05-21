fea0242d3c25c8cec77edd375b63303284e4bcec
Karl Mae <karl.m2e@gmail.com>
2016-05-21 16:12:20 +0300

    #109293170 Admin can send cards from admin panel running on local server to local or live version tournament feed

--- js/manage.js
+++ js/manage.js
@@ -3,17 +3,32 @@ $(function() {
   $('#submit-card').on('click', function(event) {
     event.preventDefault();
 
+    if ($('#card-title').val() === "") {
+      $('.create-card-notification').text("Title field can't be empty!");
+      $('#card-title').focus();
+      return false;
+    } else if ($('#card-description').val() === "") {
+      $('.create-card-notification').text("Description field can't be empty!");
+      $('#card-description').focus();
+      return false;
+    }
+
+
     $.ajax({
       type: "POST",
       url: "rest/card",
       data: JSON.stringify({
         "title": $("#card-title").val(),
-        "description": "default description"
+        "description": $("#card-description").val()
       }),
       contentType: "application/json; charset=utf-8",
       dataType: "json",
       success: function(data) {
-        found = true;
+        $('.create-card-notification').text("Card creation successful!");
+        $('#add-card').each(function() {
+          this.reset();
+        });
+        return false;
       }
     })
 
