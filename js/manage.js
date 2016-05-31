9d19b1b5102425db6af0b8ef23ede5728322a3dc
Karl Mae <karl.m2e@gmail.com>
2016-05-31 15:54:24 +0300

    #109294044 Admin can send a timer card to tournament feed

--- js/manage.js
+++ js/manage.js
@@ -34,4 +34,40 @@ $(function() {
 
   });
 
+  $('#submit-timercard').on('click', function(event) {
+    event.preventDefault();
+    
+    if ($('#timercard-title').val() === "") {
+      $('.create-timercard-notification').text("Title field can't be empty!");
+      $('#timercard-title').focus();
+      return false;
+    } else if ($('#timercard-description').val() === "") {
+      $('.create-timercard-notification').text("Description field can't be empty!");
+      $('#timercard-description').focus();
+      return false;
+    }
+    
+    var enddate = new Date(($('#round-start-time').val())).toISOString().slice(0, 19).replace('T', ' ');
+    console.log(enddate);
+    
+    $.ajax({
+      type: "POST",
+      url: "rest/timercard",
+      data: JSON.stringify({
+        "title" : $("#timercard-title").val(),
+        "enddate": enddate,
+        "description": $("#timercard-description").val()
+      }),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data) {
+        $('#add-timercard').each(function() {
+          this.reset();
+        });
+        return false;
+      }
+    })
+
+  });
+
 });
