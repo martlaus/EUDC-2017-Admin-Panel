6f1df01a2b4ab1d9a23b65b317263aa4e6a95328
wanaryytel <hanno@thorgate.eu>
2016-12-13 19:18:59 +0200

    Fixed api url

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 var API_URL = 'http://188.166.104.203:7070/rest'; //document.location.origin.replace(":8875", "") + ':7070/rest'; //188.166.104.203:7070 voi localhost
-var API_URL = 'http://localhost:7070/rest';
+// var API_URL = 'http://localhost:7070/rest';
 console.log(document.location.origin);
 var TOKEN_COOKIE_NAME = 'eudc-admin-token';
 var EMAIL_COOKIE_NAME = 'user-email';
