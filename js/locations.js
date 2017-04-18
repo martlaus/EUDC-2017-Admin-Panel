6fa6f5129c4b404990faef28f936409536c064c8
Karl Mae <karl.m2e@gmail.com>
2017-04-18 17:52:22 +0300

    #143799299 Admin can add new round location images to the server from admin panel

--- js/locations.js
+++ js/locations.js
@@ -9,18 +9,18 @@ var map,
 $.ajax({
     url: API_URL + '/location',
     dataType: 'json',
-    success: function(data) {
+    success: function (data) {
 
-        $.each(data, function(key, value) {
-            var lat  = value.lat,
-                lng  = value.lng,
+        $.each(data, function (key, value) {
+            var lat = value.lat,
+                lng = value.lng,
                 name = value.name,
-                id   = value.id;
+                id = value.id;
 
             $('.table--locations__body').append('<tr data-id="' + id + '" data-lat="' + lat + '" data-lng="' + lng + '"><td>' + name + '</td><td class="button-padding"><div class="table--locations__button table--locations__button--view">View on map</div></td><td class="button-padding"><div class="table--locations__button table--locations__button--delete"><i class="ion-trash-a ion"></i>Delete</div></td></tr>');
         });
 
-        $('.table--locations__button--view').on('click', function() {
+        $('.table--locations__button--view').on('click', function () {
             var dataLat = $(this).closest('tr').data('lat'),
                 dataLng = $(this).closest('tr').data('lng');
 
@@ -31,21 +31,56 @@ $.ajax({
             map.setCenter(markerLocation);
         });
 
-        $('.table--locations__button--delete').on('click', function() {
+        $('.table--locations__button--delete').on('click', function () {
             var $location = $(this).closest('tr'),
                 dataId = $(this).closest('tr').data('id');
 
             $.ajax({
-                url: API_URL + '/location/' + dataId ,
+                url: API_URL + '/location/' + dataId,
                 type: 'DELETE',
-                success: function(data) {
-                  console.log("Tehtud!");
-                  $location.remove();
+                success: function (data) {
+                    console.log("Tehtud!");
+                    $location.remove();
                 }
             });
         });
     }
 });
+
+$.ajax({
+    url: API_URL + '/roundlocation',
+    dataType: 'json',
+    success: function (data) {
+        var imgurl, name, id;
+
+        $.each(data, function (key, value) {
+            imgurl = value.imgurl,
+                name = value.name,
+                id = value.id;
+
+            $('.table--rlocations__body').append('<tr data-id="' + id + '" data-name="' + name + '" data-imgurl="' + imgurl + '"><td>' + name + '</td><td class="button-padding"><div class="table--locations__button table--locations__button--view">View image</div></td><td class="button-padding"><div class="table--locations__button table--locations__button--delete"><i class="ion-trash-a ion"></i>Delete</div></td></tr>');
+        });
+
+        $('.table--locations__button--view').on('click', function () {
+            window.open(imgurl);
+        });
+
+        $('.table--locations__button--delete').on('click', function () {
+            var $roundLocation = $(this).closest('tr'),
+                dataId = $(this).closest('tr').data('id');
+
+            $.ajax({
+                url: API_URL + '/roundlocation/' + dataId,
+                type: 'DELETE',
+                success: function (data) {
+                    console.log("Tehtud!");
+                    $roundLocation.remove();
+                }
+            });
+        });
+    }
+});
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -56,7 +91,7 @@ function initMap() {
         zoom: 14
     });
 
-    map.addListener('click', function(event) {
+    map.addListener('click', function (event) {
         placeMarker(event.latLng);
     });
 }
@@ -85,7 +120,7 @@ function placeMarker(location) {
     markers.push(marker);
 }
 
-$('#submit-card').on('click', function(event) {
+$('#submit-card').on('click', function (event) {
     event.preventDefault();
 
     if ($('#card-title').val() === "") {
@@ -108,9 +143,9 @@ $('#submit-card').on('click', function(event) {
         }),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function(data) {
+        success: function (data) {
             $('.create-card-notification').text("Location has been added successfully!");
-            $('#add-card').each(function() {
+            $('#add-card').each(function () {
                 this.reset();
             });
             clearMarkers();
@@ -119,6 +154,55 @@ $('#submit-card').on('click', function(event) {
                 lat: 0,
                 lng: 0
             };
+
+            return false;
+        }
+    })
+
+});
+
+$('#submit-round-location').on('click', function (event) {
+    event.preventDefault();
+
+    function isURL(str) {
+        var pattern = new RegExp('^(https?:\\/\\/)?' +
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' +
+            '((\\d{1,3}\\.){3}\\d{1,3}))' +
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+            '(\\?[;&a-z\\d%_.~+=-]*)?' +
+            '(\\#[-a-z\\d_]*)?$', 'i');
+        return pattern.test(str);
+    }
+
+    if ($('#round-location-title').val() === "") {
+        $('.create-card-notification').text("Name field can't be empty!");
+        $('#round-location-title').focus();
+        return false;
+    } else if ($('#round-location-imgurl').val() === "") {
+        $('.create-card-notification').text("URL field can't be empty!");
+        $('#round-location-imgurl').focus();
+        return false;
+    } else if (!isURL($('#round-location-imgurl').val())) {
+        $('.create-card-notification').text("URL must be correct!");
+        $('#round-location-imgurl').focus();
+        return false;
+    }
+
+
+    $.ajax({
+        type: "POST",
+        url: API_URL + '/roundlocation',
+        data: JSON.stringify({
+            "name": $('#round-location-title').val(),
+            "imgurl": $('#round-location-imgurl').val(),
+        }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            $('.create-card-notification').text("Round Location has been added successfully!");
+            $('#add-card').each(function () {
+                this.reset();
+            });
 
             return false;
         }
